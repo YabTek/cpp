66b5c92ef22e5e6c167c3c545130276b43b865fc
YabTek <yeabsira.tekuamwork@a2sv.org>
2026-04-06 01:09:30 +0200

    bst

---
+++ 96. Unique Binary Search Trees.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int node = 2; node <= n; node++){
+            int tot = 0;
+            for(int root = 1; root <= node; root++){
+                int left = root - 1;
+                int right = node - root;
+
+                tot += dp[left] * dp[right];
+            }
+            dp[node] = tot;
+        }
+
+        return dp[n];
+        
+    }
+};
