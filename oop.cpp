71982839ede880e71c81abd34e208b8e2f4ac571
YabTek <yeabsira.tekuamwork@a2sv.org>
2026-04-10 17:24:09 +0200

    oop

--- oop.cpp
+++ oop.cpp
@@ -7,11 +7,22 @@ class Animal{
         string name;
         int age;
 
+    //We can use 1 or 2 of the following constructor method
+    //1. constructor and direct initialization
+    // Animal() : name(""), age(0) {};
+    // Animal(string name, int age) : name(name), age(age) {};
+
+    //2. constructor and assignment
+    Animal(string name, int age){
+        this->name = name;
+        this->age = age;
+    };
+
     public:
         string getName(){
             return name;
         }
-        string getType(){
+        virtual string getType(){
             return "Animal";
         }
         string setAge(int newAge){
@@ -19,29 +30,38 @@ class Animal{
             return "updated age: " + to_string(age);
         }
 
+        virtual ~Animal() {};
+
 };
 
 class Dog : public Animal{
     public:
-        string getType(){
+        Dog(string name, int age) : Animal(name, age) {}
+
+        string getType() override{
             return "Dog";
         }
 
-    //constructor
-    Dog(string name, int age){
-        this->name = name;
-        this->age = age;
-    }
+        
 };
 
 int main(){
-    Dog myDog("papii", 7);
+    Dog myDog("papi", 7);
     // myDog.name = "papi";
     cout << "my dog's name is: " << myDog.getName() <<endl;
     // myDog.setAge(5);
     cout << "my dog's age is: " << myDog.age << endl;
     cout << "my dog's type is: " << myDog.getType() << endl;
 
+    //use of virtual func
+    Animal* myDog2 = new Dog("papoo", 9);
+    //since we have a pointer we should use ->
+    cout << "my second dog name is: " << myDog2->getName() <<endl;
+    //this will print animal instead of dog, becuse the compiler will see the type and decide to call the getType function in the animal class, to fix this we need to make the getType function virtual in the animal class
+    cout << "my second dog type is: " << myDog2->getType() << endl;
+
+    //creating destructor for animal class helps us to free up memory 
+    delete myDog2;
 
     return 0;
 
