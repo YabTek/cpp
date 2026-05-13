62f84eaf9e6afefe64b40af59024f7baea96b672
YabTek <90649406+YabTek@users.noreply.github.com>
2026-05-13 11:54:38 +0200

    hello

--- bubbleSort.go
+++ bubbleSort.go
@@ -1,48 +1,7 @@
 package main
 
-import ("fmt"
-	"bufio"
-	"os"
-	"strings"
-	"strconv")
+import "fmt"
 
-func swap(slice []int, j int){
-	slice[j], slice[j+1] = slice[j+1], slice[j]
-}
-
-func bubbleSort(slice []int) {
-	n := len(slice)
-
-	for i := 0; i < n-1; i++{
-		for j := 0; j < n-i-1; j++{
-			if slice[j] > slice[j+1]{
-				swap(slice, j)
-			}
-		}
-	}
-}
-
-func main(){
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Enter numbers")
-
-	scanner.Scan()
-	strNumbers := scanner.Text()
-
-	slice := []int{}
-	numbers := strings.Fields(strNumbers)
-
-	for _, num := range numbers {
-		intnum, err := strconv.Atoi(num)
-
-		if err == nil {
-			slice = append(slice, intnum)
-		}
-	}
-	bubbleSort(slice)
-
-	for _, num := range slice{
-		fmt.Printf("%d ", num)
-	}
-
+func main() {
+    fmt.Println("Hello world")
 }
